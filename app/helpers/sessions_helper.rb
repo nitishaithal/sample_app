67a49577de01c2051d9f17e08fa52e6854b17be1
nitishaithal <nitish.k008@gmail.com>
2015-02-11 07:49:08 +0000

    User editing, updating, index, admin destroy actions with errors

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,21 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+   # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+  
+# Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 
   # Returns the user corresponding to the remember token cookie.
   def current_user
@@ -44,17 +59,28 @@ module SessionsHelper
     @current_user = nil
   end
   # Returns the user according to the remember me cook
-# def current_user
- #   if (user_id = session[:user_id])
-  #    @current_user ||= User.find_by(id: user_id)
-   # elsif (user_id = cookies.signed[:user_id])
-    #  raise       # The tests still pass, so this branch is currently untested.
-     # user = User.find_by(id: user_id)
-      #if user && user.authenticated?(cookies[:remember_token])
-       # log_in user
-      #  @current_user = user
-      #end
-    #end
-  #end
+ def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      raise       # The tests still pass, so this branch is currently untested.
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
 end
